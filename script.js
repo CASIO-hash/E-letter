313767f891dc7b9ab027eb9d38e273b8a16786ec
CASIO-hash <kota_m241064cs@nitc.ac.in>
2025-03-04 17:10:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,8 +19,8 @@ document.addEventListener("DOMContentLoaded", function () {
       <strong>Hi, ${userName}.</strong> Sooo , you DO revisit!?😊 , Noice😁. <br></br>
 I want to visit soo many places with you. My Emerald greeeen!<br></br>
 
-      <strong>I see you and my heart skips a beat 💓💗 </strong>
-  `;
+      <strong>I see you and my heart skips a beat 💓💗 </br>
+Hmm... Last song was not in good quality, Here's one which i listen to when the heavens let water loose </strong>;
 
   // Play/Pause toggle functionality
   const playButton = document.getElementById("playButton");
