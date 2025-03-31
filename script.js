37fdeb607026ae204395afbf2d082cea4761acbb
CASIO-hash <kota_m241064cs@nitc.ac.in>
2025-03-31 17:02:46 +0530

    script.js

--- script.js
+++ script.js
@@ -59,6 +59,6 @@ playButton.addEventListener('click', () => {
 
 // Play song
 function playSong() {
-  const audio = new Audio('Barish.mp3'); // Replace with your song file
+  const audio = new Audio('FindingYou.mp3'); // Replace with your song file
   audio.play();
 }
